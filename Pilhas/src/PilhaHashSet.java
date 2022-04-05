c3bce47c076c5358c53fe6a873b2f2d5d9c60420
eliseu gili <gili.eliseup@gmail.com>
2022-04-05 10:00:00 -0300

    updated

--- Pilhas/src/PilhaHashSet.java
+++ Pilhas/src/PilhaHashSet.java
@@ -6,91 +6,91 @@ public class PilhaHashSet {
 
 	private Set<Object> elements;
 	private int lastElement;
-	
+
 	public PilhaHashSet() {
 		this.elements = new HashSet<Object>(10);
 		this.lastElement = -1;
 	}
-	
+
 	public PilhaHashSet(int size) {
 		this.elements = new HashSet<Object>(size);
 		this.lastElement = -1;
 	}
-	
+
 	public PilhaHashSet(PilhaHashSet pilha) throws Exception {
-		
-		if(pilha == null)
+
+		if (pilha == null)
 			throw new Exception("Invalid object value");
-		
+
 		pilha.elements = this.elements;
 		pilha.lastElement = this.lastElement;
 	}
-	
-public void push(Object obj) throws Exception {
-		
-		if(!this.equals(obj) || obj == null)
+
+	public void push(Object obj) throws Exception {
+
+		if (!this.equals(obj) || obj == null)
 			throw new Exception("Cannot insert a diferent object type in the stack");
-		
+
 		lastElement++;
 		elements.add(obj);
 	}
-	
-	public Object pop () throws Exception {
 
-		if(isEmpty())
+	public Object pop() throws Exception {
+
+		if (isEmpty())
 			throw new Exception("Can´t remove of empty stack");
-		
+
 		Object obj = elements.remove(elements.size() - 1);
-		
+
 		lastElement--;
-		
+
 		return obj;
 	}
-	
+
 	public boolean isEmpty() {
 		return elements.size() == 0;
 	}
-	
+
 	public boolean isFull() {
 		return lastElement == elements.size();
 	}
-	
+
 	public Object peek() {
 		return !this.isEmpty() ? lastElement : null;
 	}
-	
+
 	@Override
 	public String toString() {
-		 return "Stack [elements= " + elements + "]";
+		return "Stack [elements= " + elements + "]";
 	}
-	
+
 	@Override
 	public int hashCode() {
-		
+
 		int ret = 31;
-		
+
 		elements.forEach((pilha) -> new ArrayList<Object>(this.elements).hashCode());
-		
+
 		ret = ret * 13 + Integer.valueOf(this.lastElement).hashCode();
-		
+
 		return ret < 0 ? -ret : ret;
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
-		
-		if(this == obj)
+
+		if (this == obj)
 			return true;
-		if(obj.getClass() != PilhaHashSet.class)
+		if (obj.getClass() != PilhaHashSet.class)
 			return false;
-		
-		PilhaHashSet pilha = (PilhaHashSet) obj; 
-		
-		if(this.elements != pilha.elements)
+
+		PilhaHashSet pilha = (PilhaHashSet) obj;
+
+		if (this.elements != pilha.elements)
 			return false;
-		if(this.lastElement != pilha.lastElement)
+		if (this.lastElement != pilha.lastElement)
 			return false;
-		
+
 		return true;
 	}
 }
