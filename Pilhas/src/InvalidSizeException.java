9f1cc2e1409d0dcf6baaf4fc701fdad852b3d0d5
eliseu gili <gili.eliseup@gmail.com>
2022-04-04 21:12:44 -0300

    first stack version

---
+++ Pilhas/src/InvalidSizeException.java
@@ -0,0 +1,11 @@
+
+public class InvalidSizeException extends Exception{
+
+	public InvalidSizeException() {
+		super();
+	}
+	
+	public InvalidSizeException(String errorMessage) {
+		super(errorMessage);
+	}
+}
---
+++ Pilhas/src/Pilha.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pilha {
+
+	private List<Object> elements;
+	private int lastElement;
+	
+	public Pilha() {
+		elements = new ArrayList<Object>(10);
+		lastElement = -1;
+	}
+	
+	public Pilha(int size) throws Exception {
+		
+		if(size < 0)
+			throw new InvalidSizeException("Invalid size");
+		
+		elements = new ArrayList<Object>(size);
+		lastElement = -1;
+	}
+	
+	public Pilha(Pilha pilha) throws Exception {
+		
+		if(pilha == null)
+			throw new Exception("Invalid object value");
+		
+		pilha.elements = this.elements;
+		pilha.lastElement = this.lastElement;
+	}
+	
+	public void push(Object obj) throws Exception {
+		
+		if(!this.equals(obj) || obj == null)
+			throw new Exception("Cannot insert a diferent object type in the stack");
+		
+		lastElement++;
+		elements.add(obj);
+	}
+	
+	private int getSize() {
+		return elements.size();
+	}
+	
+	public Object pop () throws Exception {
+
+		if(isEmpty())
+			throw new Exception("Can´t remove of empty stack");
+		
+		Object obj = elements.remove(getSize() - 1);
+		
+		lastElement--;
+		
+		return obj;
+	}
+	
+	public boolean isEmpty() {
+		return getSize() == 0;
+	}
+	
+	public boolean isFull() {
+		return lastElement == getSize();
+	}
+	
+	public Object top() {
+		return !this.isEmpty() ? lastElement : null;
+	}
+	
+	@Override
+	public String toString() {
+		 return "Stack [elements= " + elements + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		
+		int ret = 31;
+		
+		elements.forEach((pilha) -> new ArrayList<Object>(this.elements).hashCode());
+		
+		ret = ret * 13 + Integer.valueOf(this.lastElement).hashCode();
+		
+		return ret < 0 ? -ret : ret;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if(this == obj)
+			return true;
+		if(obj.getClass() != Pilha.class)
+			return false;
+		
+		Pilha pilha = (Pilha) obj; 
+		
+		if(this.elements != pilha.elements)
+			return false;
+		if(this.lastElement != pilha.lastElement)
+			return false;
+		
+		return true;
+	}
+}
