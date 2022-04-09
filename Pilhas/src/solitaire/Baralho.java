a088118fcb753bb61ffb2556817580966dadbad5
eliseu gili <gili.eliseup@gmail.com>
2022-04-09 12:57:15 -0300

    baralho

--- Pilhas/src/solitaire/Baralho.java
+++ Pilhas/src/solitaire/Baralho.java
@@ -1,32 +1,42 @@
 package solitaire;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Baralho {
 
-	final private static String[] NAIPE = { "OUROS", "ESPADAS", "COPAS", "PAUS" };
-	final private static char[] NAIPE = { ' ', '\u2666', '\u2663', '\u2665', '\u2660' };
+//	final private static String[] NAIPE = { "OUROS", "ESPADAS", "COPAS", "PAUS" };
+//	final private static char[] NAIPE = { ' ', '\u2666', '\u2663', '\u2665', '\u2660' };
 
-
-	public Baralho() {
-//		colecaoCartas = new HashMap<>(52); // 4 naipes * 13 valores = 52
-
-//		colecaoCartas.forEach((n, v) -> colecaoCartas.put(n, v));
-	}
-
-//	@Override
-//	public String toString() {
-//		return "" + colecaoCartas.values();
-//	}
 }
 
 //pq caralhos eu não consigo trabalhar com a porra de um enum
+
+
+class Carta{
+	private Map<Naipe, Valor> colecaoCartas;
+	
+	public Carta(){
+		colecaoCartas = new HashMap<>(52); // 4 naipes * 13 valores = 52
+		
+		colecaoCartas.forEach((n, v) -> colecaoCartas.put(n, v));
+	}
+	
+	public Map<Naipe, Valor> getCartas() {
+		return Collections.unmodifiableMap(colecaoCartas);
+	}
+	
+	@Override
+	public String toString() {
+		return "" + colecaoCartas.values();
+	}
+}
 
 enum Naipe{
 	OUROS, ESPADAS, COPAS, PAUS;
 }
 
-enum VALOR{
-	
+enum Valor{
+	AS, DOIS, TRES, QUATRO, CINCO, SEIS, SETE, OITO, NOVE, DEZ, J, Q, K;
 }
